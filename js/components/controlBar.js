a22816ef7fa3feb133dfb28a4a4fc5fa5d103943
Kuu Miyazaki <miyazaqui@gmail.com>
2016-05-24 14:29:51 +0900

    Remove scrubber bar

--- js/components/controlBar.js
+++ js/components/controlBar.js
@@ -417,7 +417,6 @@ var ControlBar = React.createClass({
 
     return (
       <div className={controlBarClass} onMouseUp={this.handleControlBarMouseUp} onTouchEnd={this.handleControlBarMouseUp}>
-        <ScrubberBar {...this.props} />
 
         <div className="oo-control-bar-items-wrapper">
           {controlBarItems}
